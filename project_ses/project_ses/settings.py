c917761d30ff98d1b014c40c4fe4b050b7302e16
august <rajex369@gmail.com>
2022-12-12 16:56:47 +0545

    models created

--- project_ses/project_ses/settings.py
+++ project_ses/project_ses/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-1zgd^oc_rfj7fcwv2^siobs$qz8^s5x+6r(lg01zokkfq)1p_2'
-
+                                                                                
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_ses'
 ]
 
 MIDDLEWARE = [
